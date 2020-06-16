0ab4446ee1405269400def777a43afc82e54e620
Mateus Ferreira <mateusmais@icloud.com>
2020-06-16 19:02:32 -0300

    Atualização StoryBoard;

---
+++ TendApp/TendApp WatchKit App/Controller/Interface/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  TendApp WatchKit Extension
+//
+//  Created by Mateus Augusto M Ferreira on 16/06/20.
+//  Copyright © 2020 Mateus Augusto M Ferreira. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ TendApp/TendApp WatchKit App/Controller/Notification/NotificationController.swift
@@ -0,0 +1,38 @@
+//
+//  NotificationController.swift
+//  TendApp WatchKit Extension
+//
+//  Created by Mateus Augusto M Ferreira on 16/06/20.
+//  Copyright © 2020 Mateus Augusto M Ferreira. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import UserNotifications
+
+
+class NotificationController: WKUserNotificationInterfaceController {
+
+    override init() {
+        // Initialize variables here.
+        super.init()
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+    override func didReceive(_ notification: UNNotification) {
+        // This method is called when a notification needs to be presented.
+        // Implement it if you use a dynamic notification interface.
+        // Populate your dynamic notification interface as quickly as possible.
+    }
+}
