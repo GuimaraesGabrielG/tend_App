df18671ffaf1ac6909987b4e2232238245ac2a4d
Deyvid <j_deyvid@live.com>
2020-06-18 14:19:24 -0300

    Adicionar o timer que aparece antes de começar o exercicio

--- TendApp/TendApp WatchKit App/Controller/Interface/StretchingController.swift
+++ TendApp/TendApp WatchKit App/Controller/Interface/StretchingController.swift
@@ -8,18 +8,36 @@
 
 import UIKit
 import WatchKit
-
 class StretchingController: WKInterfaceController {
 
-    @IBOutlet weak var timerBeforeStartStretching: WKInterfaceTimer!
+    @IBOutlet weak var timerBeforeStartStretching: WKInterfaceLabel!
+    public var myTimer: Timer!
+    public var count = 5
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        timerBeforeStartStretching.setDate(Date(timeIntervalSinceNow: 4))
-        timerBeforeStartStretching.start()
+        
+        WKInterfaceDevice.current().play(.start)
+        timerBeforeStartStretching.setText("\(count)")
+        myTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: teste(myTimer:))
+
+        
+        
         
     }
     
     override func willActivate() {
         
     }
+    
+    func teste(myTimer: Timer) {
+        if count > 0 {
+            count-=1
+            timerBeforeStartStretching.setText("\(count)")
+        }
+        print(count)
+        
+        
+    }
+    
+    
 }
